3859b21e1594b535fec5e383a5101f3ea0671ebc
peteroden <pete.roden@live.com>
2016-11-23 15:46:32 -0500

    added websocket to http server

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const http = require('http')
 
 const port = process.env.PORT || 3000;
 
@@ -6,6 +6,18 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
+});
+
+var WebSocketServer = require('ws').Server
+var wss = new WebSocketServer({ server: server });
+
+wss.on('connection', function connection(ws) {
+ 
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+ 
+  ws.send('something');
 });
 
 server.listen(port);
