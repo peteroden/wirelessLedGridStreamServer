bac98c3d77399ab36e719ea49e0d175440c8bd0f
peteroden <pete.roden@live.com>
2016-11-23 15:31:52 -0500

    added http server

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port);
